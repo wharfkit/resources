92b968b2fd2c1be1617b99277739b25ff69cb262
Aaron Cox <aaron.cox@greymass.com>
2021-03-03 12:22:36 -0500

    Refactor states (#1)
    * WIP
    
    * Migrated REX to new refactored structs

--- src/rex.ts
+++ src/rex.ts
@@ -1,7 +1,50 @@
-import {Asset} from '@greymass/eosio'
-
 import {Resources, SampleUsage} from './'
-import {REXState} from './abi-types'
+
+import {Asset, Struct, UInt64, UInt8} from '@greymass/eosio'
+
+@Struct.type('rexstate')
+export class REXState extends Struct {
+    @Struct.field('uint8') version!: UInt8
+    @Struct.field('asset') total_lent!: Asset
+    @Struct.field('asset') total_unlent!: Asset
+    @Struct.field('asset') total_rent!: Asset
+    @Struct.field('asset') total_lendable!: Asset
+    @Struct.field('asset') total_rex!: Asset
+    @Struct.field('asset') namebid_proceeds!: Asset
+    @Struct.field('uint64') loan_num!: UInt64
+
+    public get reserved() {
+        return Number(this.total_lent.units) / Number(this.total_lendable.units)
+    }
+
+    public get symbol() {
+        return this.total_lent.symbol
+    }
+
+    price_per_ms(usage: SampleUsage, ms = 1): Asset {
+        return this.price_per_us(usage, ms * 1000)
+    }
+
+    price_per_us(usage: SampleUsage, us = 1000): Asset {
+        // Sample token units
+        const tokens = Asset.fromUnits(10000, this.symbol)
+
+        // Spending 1 EOS (10000 units) on REX gives this many tokens
+        const bancor = Number(tokens.units) / (this.total_rent.value / this.total_unlent.value)
+
+        // The ratio of the number of tokens received vs the sampled cpu values
+        const microseconds = bancor * usage.cpu
+
+        // The token units spent per microsecond
+        const permicrosecond = Number(tokens.units) / microseconds
+
+        // Multiply the per microsecond cost by the us requested
+        const cost = permicrosecond * us
+
+        // Converting to an Asset
+        return Asset.fromUnits(cost, this.symbol)
+    }
+}
 
 export class REXAPI {
     constructor(private parent: Resources) {}
@@ -14,36 +57,5 @@ export class REXAPI {
             type: REXState,
         })
         return response.rows[0]
-    }
-
-    get_reserved(state: REXState) {
-        const {total_lent, total_lendable} = state
-        return Number(total_lent.units) / Number(total_lendable.units)
-    }
-
-    get_price_per_ms(state: REXState, usage: SampleUsage, ms = 1): Asset {
-        // REX Values
-        const {total_rent, total_unlent} = state
-
-        // Sample token units
-        const tokens = Asset.fromUnits(10000, this.parent.symbol)
-
-        // Spending 1 EOS (10000 units) on REX gives this many tokens
-        const bancor = Number(tokens.units) / (total_rent.value / total_unlent.value)
-
-        // The ratio of the number of tokens received vs the sampled cpu values
-        const microseconds = bancor * usage.cpu
-
-        // The token units spent per microsecond
-        const permicrosecond = Number(tokens.units) / microseconds
-
-        // Converting to milliseconds
-        const permillisecond = permicrosecond * 1000
-
-        // Multiply the per millisecond cost by the ms requested
-        const cost = permillisecond * ms
-
-        // Converting to an Asset
-        return Asset.fromUnits(cost, this.parent.symbol)
     }
 }
