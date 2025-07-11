8c5c0704f3a380b1ea93e716c6099dca2d1f325b
aaroncox <aaron.cox@greymass.com>
2025-07-11 11:29:15 -0700

    Migrating some math to Antelope integers

--- src/powerup/abstract.ts
+++ src/powerup/abstract.ts
@@ -54,10 +54,10 @@ export abstract class PowerUpStateResource extends Struct {
     // Common casting for typed values to numbers
     cast() {
         return {
-            adjusted_utilization: Number(this.adjusted_utilization),
+            adjusted_utilization: String(this.adjusted_utilization),
             decay_secs: Number(this.decay_secs.value),
             exponent: Number(this.exponent),
-            utilization: Number(this.utilization),
+            utilization: String(this.utilization),
             utilization_timestamp: Number(this.utilization_timestamp.value),
             weight: new BN(String(this.weight)),
             weight_ratio: Number(this.weight_ratio),
@@ -90,13 +90,13 @@ export abstract class PowerUpStateResource extends Struct {
     }
 
     // Mimic: https://github.com/EOSIO/eosio.contracts/blob/d7bc0a5cc8c0c2edd4dc61b0126517d0cb46fd94/contracts/eosio.system/src/powerup.cpp#L274-L280
-    price_integral_delta(start_utilization: number, end_utilization: number): number {
+    price_integral_delta(start_utilization: Int64, end_utilization: Int64): number {
         const {exponent, weight} = this.cast()
         const max_price: number = this.max_price.value
         const min_price: number = this.min_price.value
         const coefficient = (max_price - min_price) / exponent
-        const start_u = new BN(start_utilization) / weight
-        const end_u = new BN(end_utilization) / weight
+        const start_u = Number(start_utilization.dividing(weight))
+        const end_u = Number(end_utilization.dividing(weight))
         const delta =
             min_price * end_u -
             min_price * start_u +
@@ -107,14 +107,18 @@ export abstract class PowerUpStateResource extends Struct {
 
     // Mimic: https://github.com/EOSIO/eosio.contracts/blob/d7bc0a5cc8c0c2edd4dc61b0126517d0cb46fd94/contracts/eosio.system/src/powerup.cpp#L262-L315
     fee(utilization_increase, adjusted_utilization) {
-        const {utilization, weight} = this.cast()
+        const {weight} = this.cast()
+        const {utilization} = this
 
-        let start_utilization: number = utilization
-        const end_utilization: number = start_utilization + utilization_increase
+        let start_utilization = Int64.from(utilization)
+        const end_utilization = start_utilization.adding(utilization_increase)
 
         let fee = 0
         if (start_utilization < adjusted_utilization) {
-            const min = Math.min(utilization_increase, adjusted_utilization - start_utilization)
+            const min = Math.min(
+                utilization_increase,
+                adjusted_utilization.subtracting(start_utilization)
+            )
             fee += Number(
                 new bigDecimal(this.price_function(adjusted_utilization) * min)
                     .divide(new bigDecimal(weight.toString()))
@@ -122,7 +126,7 @@ export abstract class PowerUpStateResource extends Struct {
             )
             start_utilization = adjusted_utilization
         }
-        if (start_utilization < end_utilization) {
+        if (start_utilization.gt(end_utilization)) {
             fee += this.price_integral_delta(start_utilization, end_utilization)
         }
         return fee
@@ -131,17 +135,18 @@ export abstract class PowerUpStateResource extends Struct {
     // Mimic: https://github.com/EOSIO/eosio.contracts/blob/d7bc0a5cc8c0c2edd4dc61b0126517d0cb46fd94/contracts/eosio.system/src/powerup.cpp#L105-L117
     determine_adjusted_utilization(options?: PowerUpStateOptions) {
         // Casting EOSIO types to usable formats for JS calculations
-        const {decay_secs, utilization, utilization_timestamp} = this.cast()
-        let {adjusted_utilization} = this.cast()
+        const {decay_secs, utilization_timestamp} = this.cast()
+        const {utilization} = this
+        let {adjusted_utilization} = this
         // If utilization is less than adjusted, calculate real time value
-        if (utilization < adjusted_utilization) {
+        if (utilization.lt(adjusted_utilization)) {
             // Create now & adjust JS timestamp to match EOSIO timestamp values
             const ts = options && options.timestamp ? options.timestamp : new Date()
             const now = TimePointSec.from(ts).toMilliseconds() / 1000
-            const diff: number = adjusted_utilization - utilization
+            const diff = adjusted_utilization.subtracting(utilization).toNumber()
             let delta: number = diff * Math.exp(-(now - utilization_timestamp) / decay_secs)
             delta = Math.min(Math.max(delta, 0), diff) // Clamp the delta
-            adjusted_utilization = utilization + delta
+            adjusted_utilization = utilization.adding(delta)
         }
         return adjusted_utilization
     }
